0f64646ed720ca753dfce3da275553ccf4bf0be2
Colton <charrill@pm.me>
2021-02-21 22:28:44 -0500

    created beta and release venv, inital program

---
+++ v0.12.0_PowerStatus.py
@@ -0,0 +1,24 @@
+from pycomm3 import CIPDriver
+
+def stratix_power_status():
+    message_path = '192.168.1.101'
+    
+    with CIPDriver(message_path) as device:
+        data = device.generic_message(
+            service=b'\x0e',
+            class_code=863,
+            instance=1,
+            attribute=8,
+            connected=True,
+            unconnected_send=False,
+            route_path=True,
+            data_format=[('Power Status', 'INT'), ],
+            name='Power Status'
+        )
+        
+        pwr_a = 'on' if data.value['Power Status'] & 0b_1 else 'off'
+        pwr_b = 'on' if data.value['Power Status'] & 0b_10 else 'off'
+        
+        print(f'PWR A: {pwr_a}, PWR B: {pwr_b}')
+        
+stratix_power_status()
